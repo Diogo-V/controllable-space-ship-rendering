c737ee625ab7fa79c9b984ac4af5cf9ae0b26bb5
Diogo-V <diogo.venancio00@gmail.com>
2022-05-28 23:23:27 +0100

    Implemented globe. In the middle of implementing a spaceship

--- src/js/main.js
+++ src/js/main.js
@@ -154,263 +154,82 @@ class Main {
   #buildScene = (scene) => {
     'use strict'
 
-    //Curved Tube
-    class CustomSinCurve extends THREE.Curve {
-      constructor(scale) {
-        super()
-        this.scale = scale
-      }
-      getPoint(t) {
-        const tx = t * 7.5 - 1.5
-        const ty = Math.sin(2 * Math.PI * t)
-        const tz = 0
-        return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale)
-      }
-    }
+    let radius
+    let geometry
+    let material
+    let widthSegments
+    let heightSegments
+    let ball
+    let height
+    let cube
+    let depth
+    let depthSegments
+    let width
+    let spaceshipBody
+    let spaceshipHead
 
-    // Left "S"
-    let path = new CustomSinCurve(10)
-    let tubularSegments = 20  // ui: tubularSegments
-    let radius = 1  // ui: radius
-    let radialSegments = 8  // ui: radialSegments
-    let closed = false  // ui: closed
-    let geometry = new THREE.TubeGeometry(
-      path, tubularSegments, radius, radialSegments, closed)
-    let material = new THREE.MeshBasicMaterial( { color: 0x40ffef } )
-    let tube = new THREE.Mesh( geometry, material )
-    tube.position.x = -95
-    tube.position.y = 65
-    tube.position.z = -25
-    scene.add(tube)
-    this.#sceneObjects.push(tube)
-
-    // Big Central Donut *
-    radius = 90  // ui: radius
-    let tubeRadius = 1  // ui: tubeRadius
-    radialSegments = 16  // ui: radialSegments
-    tubularSegments = 40  // ui: tubularSegments
-    geometry = new THREE.TorusGeometry(
-      radius, tubeRadius,
-      radialSegments, tubularSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xffff00 } )
-    let torus = new THREE.Mesh( geometry, material )
-    torus.position.x = 0
-    torus.position.y = 0
-    torus.position.z = 0
-    scene.add(torus)
-    this.#sceneObjects.push(torus)
-
-    // Big Central Pyramid
-    radius = 40  // ui: radius
-    let height = 105  // ui: height
-    radialSegments = 4  // ui: radialSegments
-    geometry = new THREE.ConeGeometry(radius, height, radialSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0x03fc20 } )
-    let pyramid = new THREE.Mesh( geometry, material )
-    pyramid.position.x = 0
-    pyramid.position.y = 0
-    pyramid.position.z = -140
-    scene.add(pyramid)
-    this.#sceneObjects.push(pyramid)
-
-    // Big Front Cube *
-    let width = 50  // ui: width
-    height = 50  // ui: height
-    let depth = 50  // ui: depth
-    let widthSegments = 4  // ui: widthSegments
-    let heightSegments = 4 // ui: heightSegments
-    let depthSegments = 4  // ui: depthSegments
-    geometry = new THREE.BoxGeometry(
-      width, height, depth,
-      widthSegments, heightSegments, depthSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xfc3d03 } )
-    let cube = new THREE.Mesh( geometry, material )
-    cube.position.x = 51.5
-    cube.position.y = -75
-    cube.position.z = 75
-    scene.add(cube)
-    this.#sceneObjects.push(cube)
-
-    // Saturn *
-    radius = 24  // ui: radius
-    widthSegments = 12  // ui: widthSegments
-    heightSegments = 8  // ui: heightSegments
+    // World
+    radius = 70
+    widthSegments = 32
+    heightSegments = 32
     geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xf1c681 } )
-    let saturn = new THREE.Mesh( geometry, material )
-    saturn.position.x = -90
-    saturn.position.y = -50
-    saturn.position.z = 35
-    scene.add(saturn)
-    this.#sceneObjects.push(saturn)
-
-    radius = 26  // ui: radius
-    tubeRadius = 1  // ui: tubeRadius
-    radialSegments = 16  // ui: radialSegments
-    tubularSegments = 24  // ui: tubularSegments
-    geometry = new THREE.TorusGeometry(
-      radius, tubeRadius,
-      radialSegments, tubularSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0x94846c } )
-    torus = new THREE.Mesh( geometry, material )
-    torus.position.x = -90
-    torus.position.y = -50
-    torus.position.z = 35
-    torus.rotation.x = Math.PI / 2
-    scene.add(torus)
-    this.#sceneObjects.push(torus)
-
-    // Right Side Ball *
-    radius = 15  // ui: radius
-    widthSegments = 12  // ui: widthSegments
-    heightSegments = 8  // ui: heightSegments
-    geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xfc03fc } )
-    let ball = new THREE.Mesh( geometry, material )
-    ball.position.x = 125
-    ball.position.y = 10
-    ball.position.z = 60
+    material = new THREE.MeshBasicMaterial({
+      map: new THREE.TextureLoader().load('./../../resources/textures/earth_base.jpg')
+    })
+    ball = new THREE.Mesh(geometry, material)
+    ball.position.x = 0
+    ball.position.y = 0
+    ball.position.z = 0
     scene.add(ball)
     this.#sceneObjects.push(ball)
 
-    // Central Ball *
-    radius = 5  // ui: radius
-    widthSegments = 12  // ui: widthSegments
-    heightSegments = 8 // ui: heightSegments
-    geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xbc40ff } )
-    ball = new THREE.Mesh( geometry, material )
-    ball.position.x = 30
-    ball.position.y = 70
-    ball.position.z = -180
-    scene.add(ball)
-    this.#sceneObjects.push(ball)
-
-    // Right Side Octahedron *
-    radius = 10  // ui: radius
-    geometry = new THREE.OctahedronGeometry(radius)
-    material = new THREE.MeshBasicMaterial( { color: 0x3d03fc } )
-    let octahedron = new THREE.Mesh( geometry, material )
-    octahedron.position.x = 70
-    octahedron.position.y = 50
-    octahedron.position.z = -85
-    scene.add(octahedron)
-    this.#sceneObjects.push(octahedron)
-
-    // Triangular Prism
-    let radiusTop = 10  // ui: radiusTop
-    let radiusBottom = 10 // ui: radiusBottom
-    height = 6  // ui: height
-    radialSegments = 3  // ui: radialSegments
-    geometry = new THREE.CylinderGeometry(
-      radiusTop, radiusBottom, height, radialSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xff7340 } )
-    let prism = new THREE.Mesh( geometry, material )
-    prism.position.x = -115
-    prism.position.y = 85
-    prism.position.z = 85
-    prism.rotation.x = Math.PI / 6
-    prism.rotation.z = Math.PI / 2
-    scene.add(prism)
-    this.#sceneObjects.push(prism)
-
-    // Small Right Side Torus *
-    radius = 10 // ui: radius
-    tubeRadius = 2.5  // ui: tubeRadius
-    radialSegments = 16  // ui: radialSegments
-    tubularSegments = 24  // ui: tubularSegments
-    geometry = new THREE.TorusGeometry(
-      radius, tubeRadius,
-      radialSegments, tubularSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0x94846c } )
-    torus = new THREE.Mesh( geometry, material )
-    torus.position.x= 80
-    torus.position.y= -27.5
-    torus.position.z= -15
-    torus.rotation.x = Math.PI / 2
-    scene.add(torus)
-    this.#sceneObjects.push(torus)
-
-    // TODO: start -> THREE OBJECTS TO MOVE
-
-    // Cone
-    radius = 5  // ui: radius
-    height = 10  // ui: height
-    radialSegments = 30  // ui: radialSegments
-    geometry = new THREE.ConeGeometry(radius, height, radialSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xfc03e3 } )
-    let cone = new THREE.Mesh( geometry, material )
-    cone.position.x = 0
-    cone.position.y = 0
-    cone.position.z = 0
-    cone.rotation.x = Math.PI
-
-    let cubesGroup = new THREE.Group()
-    // Cube on Cube
+    // Orbital trash
     width = 4  // ui: width
     height = 4  // ui: height
     depth = 4  // ui: depth
     widthSegments = 5  // ui: widthSegments
     heightSegments = 5  // ui: heightSegments
     depthSegments = 5  // ui: depthSegments
-    geometry = new THREE.BoxGeometry(
-      width, height, depth,
-      widthSegments, heightSegments, depthSegments)
+    geometry = new THREE.BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments)
     material = new THREE.MeshBasicMaterial( { color: 0xfc9803 } )
     cube = new THREE.Mesh( geometry, material )
     cube.position.x = 0
-    cube.position.y = -18
+    cube.position.y = -90
     cube.position.z = 0
-    cubesGroup.add(cube)
+    scene.add(cube)
+    this.#sceneObjects.push(cube)
 
-    width = 20  // ui: width
-    height = 4  // ui: height
-    depth = 20 // ui: depth
-    widthSegments = 3  // ui: widthSegments
-    heightSegments = 3  // ui: heightSegments
-    depthSegments = 3 // ui: depthSegments
-    geometry = new THREE.BoxGeometry(
-      width, height, depth,
-      widthSegments, heightSegments, depthSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xfc9803 } )
-    cube = new THREE.Mesh( geometry, material )
-    cube.position.x = 0
-    cube.position.y = -22
-    cube.position.z = 0
-    cubesGroup.add(cube)
+    // Spaceship
+    geometry = new THREE.CylinderGeometry(5, 5, 20, 32)
+    material = new THREE.MeshBasicMaterial({color: 0xffff00})
+    spaceshipBody = new THREE.Mesh(geometry, material)
+    spaceshipBody.position.x = 120
 
+    geometry = new THREE.CylinderGeometry(2, 2, 8, 32)
+    material = new THREE.MeshBasicMaterial({color: 0xffff00})
+    spaceshipHead = new THREE.Mesh(geometry, material)
+    spaceshipHead.position.y = 14
+    spaceshipBody.add(spaceshipHead)
 
-    radius = 3  // ui: radius
-    widthSegments = 12  // ui: widthSegments
-    heightSegments = 8 // ui: heightSegments
-    geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments)
-    material = new THREE.MeshBasicMaterial( { color: 0xbc40ff } )
-    ball = new THREE.Mesh( geometry, material )
-    ball.position.x = -45
-    ball.position.y = 0
-    ball.position.z = 0
+    scene.add(spaceshipBody)
+    this.#sceneObjects.push(spaceshipBody)
 
-    scene.add(this.getCompound().getGroup())
-    this.#sceneObjects.push(this.getCompound().getGroup())
-
+    // TODO: might be needed in the future
     // Setting pivot point
-    let pivotPoint1 = new THREE.Object3D();
-    pivotPoint1.position.set(0,0,-45);
-    pivotPoint1.add(cone);
-    this.getCompound().setPrimary(pivotPoint1);
-
-    let pivotPoint2 = new THREE.Object3D();
-    pivotPoint2.position.set(0,0,0);
-    pivotPoint2.add(cubesGroup);
-    this.getCompound().setSecondary(pivotPoint2);
-
-    let pivotPoint3 = new THREE.Object3D();
-    pivotPoint3.position.set(0,-20,0);
-    pivotPoint3.add(ball);
-    this.getCompound().setTertiary(pivotPoint3);
-
-
-    // TODO: end -> objects to move
+    // let pivotPoint1 = new THREE.Object3D()
+    // pivotPoint1.position.set(0,0,-45)
+    // pivotPoint1.add(cone)
+    // this.getCompound().setPrimary(pivotPoint1)
+    //
+    // let pivotPoint2 = new THREE.Object3D()
+    // pivotPoint2.position.set(0,0,0)
+    // pivotPoint2.add(cubesGroup)
+    // this.getCompound().setSecondary(pivotPoint2)
+    //
+    // let pivotPoint3 = new THREE.Object3D()
+    // pivotPoint3.position.set(0,-20,0)
+    // pivotPoint3.add(ball)
+    // this.getCompound().setTertiary(pivotPoint3)
 
   }
 
